6e742787c41a247c7035632a8609eeb102dd29dc
Vincenzo <108551966+Vincas2539@users.noreply.github.com>
2022-12-11 13:28:54 -0300

    Organizando notebook e arrumando encoder

--- RedeNeural/package/encoder.py
+++ RedeNeural/package/encoder.py
@@ -8,55 +8,36 @@ from os.path import isfile, join
 
 encoders = {}
 
-def encode_column(series : pd.Series, name : str):
-    """Encode one column
 
-    Args:
-        series (pd.Series): pd.series or array to be encoded.
-        name (str): name of column to be possible to save encoder.
-
-    Returns:
-        pd.Series : encoded columns 
-    """    
-    if name not in encoders:
-        encoders[name] = preprocessing.LabelEncoder()
-    res = encoders[name].fit_transform(series)
-    return res
-
-
-def decode_column(series : pd.Series, name : str):
-    """Decode column
+def decode_DataFrame(series : pd.Series, name : str):
+    """Decode DataFrame
 
     Args:
         series (pd.Series): pd.series or array to be decoded.
-        name (str): name of column, to find encoder.
+        name (str): name of DataFrame, to find encoder.
 
     Returns:
         pd.Series : decoded column
     """    
     if name not in encoders:
-        encoders[name] = preprocessing.LabelEncoder()
+        encoders[name] = preprocessing.MinMaxScaler(feature_range = (-1,1))
     res = encoders[name].inverse_transform(series)
     return res
 
 
 ##Decoding all char columns
-def encode_DataFrame(df : pd.DataFrame):
-    """Encodes all text dataFrame columns transforming them into numeric.
+def encode_DataFrame(df : pd.DataFrame, name : str):
+    """Encodes all dataFrame columns transforming into range (-1, 1).
 
     Args:
         df (pd.DataFrame): DataFrame to transform. 
-
+        name (string): DataFrame name.
     Returns:
         pd.DataFrame: Transformed DataFrame.
     """    
-
-    str_columns = df.select_dtypes(include=[object]).columns
-    for value in str_columns:
-        try:
-            df[value] = encode_column(df[value], value)
-        except:
-            pass
+    if name not in encoders:
+        encoders[name] = preprocessing.MinMaxScaler(feature_range = (-1,1))
+    df = encoders[name].fit_transform(df)
             
     return df
 
@@ -64,7 +45,7 @@ def save_encoders():
     """Save encoders fitted so it doesnt need to fit next time. 
     """    
     for name_encoder in encoders:
-        np.save(os.path.join('package/encoders', name_encoder), encoders[name_encoder].classes_)
+        np.save(os.path.join('package/encoders', name_encoder), encoders[name_encoder])
 
 
 def load_encoders():
@@ -72,9 +53,9 @@ def load_encoders():
     """    
     onlyfiles = [f for f in listdir('package/encoders') if isfile(join('package/encoders', f))]
     for name_encoder in onlyfiles:
-        encoders[name_encoder] = preprocessing.LabelEncoder()
+        encoders[name_encoder] = preprocessing.MinMaxScaler(feature_range = (-1,1))
         encoders[name_encoder].classes_ = np.load(os.path.join('package/encoders', name_encoder), allow_pickle=True)
 
-    print("Loaded ", len(onlyfiles), "encoders.")\
+    print("Loaded ", len(onlyfiles), "encoders.")
 
 load_encoders()
